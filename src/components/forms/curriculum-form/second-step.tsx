12e7e1b6f0d6be3a13debc7a6a708cff3c84edd1
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-12-30 17:09:26 -0300

    chore: organize curriculum steps

--- src/components/forms/curriculum-form/second-step.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface SecondStepProps {
-  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
-}
-
-export default function SecondStep(props: SecondStepProps) {
-  return <div>SecondStep</div>;
-}
