927a5c449ec3e9f88c8cb2fc79e12945705ff71a
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-12-30 17:09:38 -0300

    feat: academic education set

---
+++ src/const/academic-education-category.ts
@@ -0,0 +1,13 @@
+export const AcademicEducation = [
+  ...new Set([
+    "Formação Acadêmica",
+    "Estágio Extracurriculares",
+    "Monitoria",
+    "Intercâmbio Univrsitário",
+    "Atividades de Extensão",
+    "Ligas Acadêmicas",
+    "Órgãos Académicos",
+    "ENADE",
+    "Língua Estrangeira",
+  ]),
+];
