1d673b5b9d7fcaf4bb5cfe62dacd37766970b760
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-12-30 13:10:01 -0300

    feat: schema first step

---
+++ src/components/forms/curriculum-form/type.ts
@@ -0,0 +1,14 @@
+export type FirstStepFormInput = {
+  name: string;
+  presentationName: string;
+  fathersName: string;
+  mothersName: string;
+  birthday: Date;
+  identityDocument: string;
+  CRM: string;
+  CPF: string;
+  phone: string;
+  address: string;
+  email: string;
+  lattes: string;
+};
