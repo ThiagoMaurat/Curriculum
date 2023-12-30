f8e3ab204a9b6ab35d635e4376c15a49560dbd99
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-12-30 17:48:53 -0300

    feat:bibliography const

---
+++ src/const/bibliography.ts
@@ -0,0 +1,9 @@
+export const Bibliography = [
+  ...new Set([
+    "Atividades de Pesquisa",
+    "Artigos Completos Publicados",
+    "Apresentação em Evento Científico",
+    "Apresentação em Palestras",
+    "Capítulo de Livro",
+  ]),
+];
