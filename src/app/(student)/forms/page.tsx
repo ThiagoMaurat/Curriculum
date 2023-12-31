4c190546175c6ac2cf5ec585f0b5cd6ed2e26ed8
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-12-31 18:01:19 -0300

    feat: noSSR hook

--- src/app/(student)/forms/page.tsx
+++ src/app/(student)/forms/page.tsx
@@ -1,10 +1,11 @@
+import { NoSSRWrapper } from "@/hooks/no-ssr-wrapper";
 import FormsTemplate from "@/templates/forms-template";
 import React from "react";
 
 export default function FormsPage() {
   return (
-    <>
+    <NoSSRWrapper>
       <FormsTemplate />
-    </>
+    </NoSSRWrapper>
   );
 }
---
+++ src/hooks/no-ssr-wrapper.tsx
@@ -0,0 +1,14 @@
+import dynamic from "next/dynamic";
+import React from "react";
+
+interface NoSSRWrapperProps {
+  children: React.ReactNode;
+}
+
+export const NoSSRWrapper = ({ children }: NoSSRWrapperProps) => (
+  <React.Fragment>{children}</React.Fragment>
+);
+
+export default dynamic(() => Promise.resolve(NoSSRWrapper), {
+  ssr: false,
+});
