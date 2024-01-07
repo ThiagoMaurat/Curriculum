7ce5388cf78434166b4965c2d5e1397b9b1f15f3
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-01-07 18:13:41 -0300

    feat: auth navbar

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import NavBar from "@/components/navbar";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
@@ -9,30 +10,36 @@ export default function layout(props: AuthLayoutProps) {
   const { children } = props;
 
   return (
-    <div className="h-screen bg-[#E7E2FF] flex items-center justify-center">
-      <div className="w-full container min-h-[600px] h-auto flex sm:flex-row flex-col my-4">
-        <main className="w-full p-4 flex items-center justify-center mx-auto rounded-s-xl bg-white">
-          {children}
-        </main>
+    <React.Fragment>
+      <div className="bg-[#E7E2FF]  dark:bg-card">
+        <NavBar />
+      </div>
 
-        <div className="relative w-full rounded-e-xl overflow-hidden flex items-center justify-center ">
-          <Image
-            className="absolute w-full"
-            height={600}
-            width={600}
-            src={"/bg-auth.svg"}
-            alt="login background image"
-          />
+      <div className=" h-[calc(100vh-96px)] bg-[#E7E2FF] dark:bg-card flex flex-col items-center justify-center">
+        <div className="container px-0 w-full  min-h-[600px] h-auto flex sm:flex-row flex-col my-4">
+          <main className="w-full p-4 flex items-center justify-center mx-auto rounded-s-xl bg-card">
+            {children}
+          </main>
 
-          <Image
-            src={"/auth-image.svg"}
-            alt="login image"
-            width={300}
-            height={300}
-            className="relative z-1 rounded-xl m-4 overflow-hidden"
-          />
+          <div className="relative w-full rounded-e-xl overflow-hidden flex items-center justify-center ">
+            <Image
+              className="absolute w-full"
+              height={600}
+              width={600}
+              src={"/bg-auth.svg"}
+              alt="login background image"
+            />
+
+            <Image
+              src={"/auth-image.svg"}
+              alt="login image"
+              width={300}
+              height={300}
+              className="relative z-1 rounded-xl m-4 overflow-hidden"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
