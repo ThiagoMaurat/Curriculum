b5333bdc2c04784995826afd75e83f9f7f8c9f43
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-01-07 18:52:24 -0300

    fix: auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,12 +10,10 @@ export default function layout(props: AuthLayoutProps) {
   const { children } = props;
 
   return (
-    <React.Fragment>
-      <div className="bg-[#E7E2FF]  dark:bg-card">
-        <NavBar />
-      </div>
+    <div className="bg-[#E7E2FF] dark:bg-card">
+      <NavBar />
 
-      <div className=" h-[calc(100vh-96px)] bg-[#E7E2FF] dark:bg-card flex flex-col items-center justify-center">
+      <div className=" h-[calc(100vh-96px)] flex flex-col items-center justify-center">
         <div className="container px-0 w-full  min-h-[600px] h-auto flex sm:flex-row flex-col my-4">
           <main className="w-full p-4 flex items-center justify-center mx-auto rounded-s-xl bg-card dark:bg-zinc-950">
             {children}
@@ -40,6 +38,6 @@ export default function layout(props: AuthLayoutProps) {
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 }
