9cc1a90be6473b0babb45926eba98d10718cbadd
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-01-09 19:23:28 -0300

    feat:ResetPasswordSecondStepUseCase use case

---
+++ src/server/use-cases/reset-password/second-step-validation/index.ts
@@ -0,0 +1,52 @@
+import { UsersRepository } from "@/server/repositories/user-repository";
+import { hash } from "bcryptjs";
+
+interface ResetPasswordSecondStepUseCaseInput {
+  email: string;
+  code: string;
+  password: string;
+  confirmPassword: string;
+}
+
+interface ResetPasswordSecondStepUseCaseOutput {
+  status: "updated" | null;
+}
+
+export class ResetPasswordSecondStepUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    code,
+    password,
+    confirmPassword,
+  }: ResetPasswordSecondStepUseCaseInput): Promise<ResetPasswordSecondStepUseCaseOutput | null> {
+    if (password !== confirmPassword) {
+      throw new Error("Senhas não conferem");
+    }
+
+    const user = await this.usersRepository.checkIfUserAndPasswordCodeMatch(
+      email,
+      code
+    );
+
+    if (!user) {
+      throw new Error("Usuário ou código inválido");
+    }
+
+    const newPassowrd = await hash(password, 6);
+
+    const resetPassword = await this.usersRepository.updateUser(
+      { password: newPassowrd },
+      user.id
+    );
+
+    if (!resetPassword) {
+      return null;
+    }
+
+    return {
+      status: "updated",
+    };
+  }
+}
