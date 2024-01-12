60b5e681ebe66cd4a112592c138f219a41382a3c
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-01-12 03:44:09 -0300

    feat:forms student page

--- src/app/(student)/forms/page.tsx
+++ src/app/(student)/forms/page.tsx
@@ -1,5 +1,11 @@
+import FormsTemplateStudent from "@/components/templates/forms-template-student";
+import { NoSSRWrapper } from "@/hooks/no-ssr-wrapper";
 import React from "react";
 
 export default function FormsPage() {
-  return <div>FormsPage</div>;
+  return (
+    <NoSSRWrapper>
+      <FormsTemplateStudent />
+    </NoSSRWrapper>
+  );
 }
