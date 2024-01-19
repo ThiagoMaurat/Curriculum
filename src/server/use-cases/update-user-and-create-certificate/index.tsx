c55384367198a9ba74ed2d3bee2603d9c0458c41
Thiago Maurat <thiagomaurat@hotmail.com>
2024-01-19 14:23:45 -0300

    feat: curriculum update user and create certificate

---
+++ src/server/use-cases/update-user-and-create-certificate/index.tsx
@@ -0,0 +1,79 @@
+import { UsersRepository } from "@/server/repositories/user-repository";
+import { CertificationsRepository } from "@/server/repositories/certification-repository";
+import {
+  SendUserCurriculum,
+  sendUserCurriculumSchema,
+} from "@/validators/send-user-curriculum";
+import { CertificationInsertSchema } from "@/db/types-schema";
+
+interface UpdateUserAndCreateCertificateInput {
+  user: SendUserCurriculum;
+  certification:
+    | Array<{
+        url: string;
+        key: string;
+      }>
+    | undefined;
+  userId: string;
+}
+
+export class UpdateUserAndCreateCertificateUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+    private certificationRepository: CertificationsRepository
+  ) {}
+
+  async execute({
+    user,
+    userId,
+    certification,
+  }: UpdateUserAndCreateCertificateInput) {
+    const userValidated = sendUserCurriculumSchema.parse(user);
+
+    if (certification && certification.length > 0) {
+      const formatCertificate: CertificationInsertSchema[] = certification.map(
+        (data) => {
+          return {
+            userId: userId,
+            url: data.url,
+            key: data.key,
+          };
+        }
+      );
+
+      const certificationCreated =
+        await this.certificationRepository.createCertifications(
+          formatCertificate
+        );
+
+      if (!certificationCreated) {
+        throw new Error("Erro ao criar certificação");
+      }
+    }
+
+    const updateUser = await this.usersRepository.updateUser(
+      {
+        name: userValidated.name,
+        presentationName: userValidated.presentationName,
+        fathersName: userValidated.fathersName,
+        mothersName: userValidated.mothersName,
+        birthday: userValidated.birthday,
+        identityDocument: userValidated.identityDocument,
+        CRM: userValidated.CRM,
+        CPF: userValidated.CPF,
+        phone: userValidated.phone,
+        address: userValidated.address,
+        email: userValidated.email,
+        selfDescription: userValidated.selfDescription,
+        lattes: userValidated.lattes,
+      },
+      userId
+    );
+
+    if (!updateUser) {
+      throw new Error("Erro ao atualizar usuário");
+    }
+
+    return updateUser;
+  }
+}
