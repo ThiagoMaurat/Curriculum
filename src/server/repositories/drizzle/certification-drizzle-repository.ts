b17728a020fdb518083740725d940b8d1a287e8b
Thiago Maurat <thiagomaurat@hotmail.com>
2024-02-02 09:55:42 -0300

    feat: create certification repository

--- src/server/repositories/drizzle/certification-drizzle-repository.ts
+++ src/server/repositories/drizzle/certification-drizzle-repository.ts
@@ -5,10 +5,29 @@ import {
 import { CertificationsRepository } from "../interfaces/certification-repository";
 import { db } from "@/server/db/drizzle";
 import { certifications } from "@/server/db/schema";
+import { and, eq } from "drizzle-orm";
 
 export class DrizzleCertificationRepository
   implements CertificationsRepository
 {
+  async deleteCertificationById(
+    key: string,
+    userId: string
+  ): Promise<Certification | null> {
+    const [certificate] = await db
+      .delete(certifications)
+      .where(
+        and(eq(certifications.key, key), eq(certifications.userId, userId))
+      )
+      .returning();
+
+    if (!certificate) {
+      return null;
+    }
+
+    return certificate;
+  }
+
   async createCertifications(
     data: CertificationInsertSchema[]
   ): Promise<Certification | null> {
