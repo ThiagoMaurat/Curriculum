deca7e8deb6887cab4920b6b29905431fab1c98b
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-02-24 15:33:07 -0300

    feat: curriculum form collaborator schema

--- src/components/forms/create-curriculum-form-collaborator/schema.ts
+++ src/components/forms/create-curriculum-form-collaborator/schema.ts
@@ -1,98 +1,49 @@
 import { z } from "zod";
 
 export const schema = z.object({
-  name: z
-    .string({ required_error: "O nome é obrigatório" })
-    .min(3, { message: "O nome deve ter pelo menos 3 letras" }),
-  fathersName: z.string({
-    required_error: "O nome do pai é obrigatório",
-  }),
-  mothersName: z.string({
-    required_error: "O nome da mão é obrigatório",
-  }),
-  birthday: z
-    .date({
-      required_error: "A data de nascimento é obrigatório",
+  academicEducation: z.array(
+    z.object({
+      initialYear: z.coerce.number().min(1900, "Insira uma data"),
+      finalYear: z.coerce.number().min(1900, "Insira uma data"),
+      subcategory: z.string().min(1, "Insira uma categoria"),
+      certifications: z.string().optional(),
+      description: z.string().min(1, "Insira uma descrição"),
     })
-    .min(new Date(1900, 0, 1), "Insira uma data de nascimento"),
-  identityDocument: z
-    .string({
-      required_error: "O RG é obrigatório",
+  ),
+  bibliography: z.array(
+    z.object({
+      initialYear: z.coerce.number().min(1900, "Insira uma data"),
+      finalYear: z.coerce.number().min(1900, "Insira uma data"),
+      subcategory: z.string().min(1, "Insira uma categoria"),
+      certifications: z.string().optional(),
+      description: z.string().min(1, "Insira uma descrição"),
     })
-    .transform((val, ctx) => {
-      const formattedVal = val
-        .replaceAll(".", "")
-        .replace("-", "")
-        .replaceAll("_", "");
-
-      if (formattedVal.length < 9) {
-        ctx.addIssue({
-          code: "custom",
-          message: "Deve possuir 9 caracteres",
-        });
-      }
-
-      return val;
-    }),
-  CRM: z
-    .string({
-      required_error: "O CRM é obrigatório",
+  ),
+  eventsCongress: z.array(
+    z.object({
+      initialYear: z.coerce.number().min(1900, "Insira uma data"),
+      finalYear: z.coerce.number().min(1900, "Insira uma data"),
+      subcategory: z.string().min(1, "Insira uma categoria"),
+      certifications: z.string().optional(),
+      description: z.string().min(1, "Insira uma descrição"),
     })
-    .optional(),
-  CPF: z
-    .string({
-      required_error: "O CPF é obrigatório",
+  ),
+  extracurricularActivities: z.array(
+    z.object({
+      initialYear: z.coerce.number().min(1900, "Insira uma data"),
+      finalYear: z.coerce.number().min(1900, "Insira uma data"),
+      subcategory: z.string().min(1, "Insira uma categoria"),
+      certifications: z.string().optional(),
+      description: z.string().min(1, "Insira uma descrição"),
     })
-    .min(11, { message: "Campo obrigatório" })
-    .transform((val, ctx) => {
-      const formattedVal = val
-        .replaceAll(".", "")
-        .replace("-", "")
-        .replaceAll("_", "");
-
-      if (formattedVal.length < 11) {
-        ctx.addIssue({
-          code: "custom",
-          message: "Deve possuir 11 caracteres",
-        });
-      }
-
-      return val;
-    }),
-  phone: z
-    .string({
-      required_error: "O telefone é obrigatório",
+  ),
+  professionalExperience: z.array(
+    z.object({
+      initialYear: z.coerce.number().min(1900, "Insira uma data"),
+      finalYear: z.coerce.number().min(1900, "Insira uma data"),
+      subcategory: z.string().min(1, "Insira uma categoria"),
+      certifications: z.string().optional(),
+      description: z.string().min(1, "Insira uma descrição"),
     })
-    .transform((val, ctx) => {
-      const formattedVal = val
-        .replaceAll("(", "")
-        .replaceAll(")", "")
-        .replaceAll("-", "")
-        .replaceAll("_", "")
-        .replaceAll(" ", "")
-        .trim();
-
-      if (formattedVal.length < 11) {
-        ctx.addIssue({
-          code: "custom",
-          message: "Deve ser no formato (DDD)99999-9999",
-        });
-      }
-
-      return formattedVal;
-    }),
-  address: z.string({
-    required_error: "O endereço é obrigatório",
-  }),
-  email: z
-    .string({
-      required_error: "O email é obrigatório",
-    })
-    .email({ message: "Email inválido" }),
-  lattes: z.string().optional(),
-  description: z
-    .string({
-      required_error: "A descrição é obrigatória",
-    })
-    .min(10, { message: "Deve ter pelo menos 10 letras" }),
+  ),
 });
