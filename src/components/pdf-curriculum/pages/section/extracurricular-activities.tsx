3f0819385b560bec9a3f63e61042f197f4413db6
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-02-24 11:50:22 -0300

    feat: extracurricular activities pdf

---
+++ src/components/pdf-curriculum/pages/section/extracurricular-activities.tsx
@@ -0,0 +1,94 @@
+import { CurriculumFormInput } from "@/components/forms/create-curriculum-form-collaborator/type";
+import React from "react";
+import { View, Text } from "@react-pdf/renderer";
+import { commonStyles } from "../../common-style";
+import { formatBySequenceForms } from "../../helpers/format-chapters-by-sequence-forms";
+import { orderBy } from "lodash";
+
+interface ExtracurricularActivitiesProps {
+  data: CurriculumFormInput;
+}
+
+export default function ExtracurricularActivities({
+  data,
+}: ExtracurricularActivitiesProps) {
+  if (
+    !data.extracurricularActivities ||
+    !data.extracurricularActivities?.[0].description
+  ) {
+    return null;
+  }
+
+  return (
+    <React.Fragment>
+      <View style={[commonStyles.commonCentralizedView, { marginTop: 32 }]}>
+        <Text style={commonStyles.chapter}>
+          {`${
+            formatBySequenceForms(data).extracurricularActivities
+          }. ATIVIDADES EXTRACURRICULARES`}
+        </Text>
+      </View>
+
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 20,
+          textAlign: "justify",
+        }}
+      >
+        {orderBy(
+          data.extracurricularActivities,
+          [(item) => item.finalYear || item.initialYear, "initialYear"],
+          ["desc", "desc"]
+        ).map((item, index) => (
+          <View
+            key={`extracurricularActivities-${index}`}
+            style={index > 0 ? { paddingTop: 30 } : undefined}
+          >
+            <Text style={[commonStyles.subtitle]}>
+              {`${formatBySequenceForms(data).extracurricularActivities}.${
+                index + 1
+              } ${item.subcategory}`}
+            </Text>
+
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 10,
+                gap: 25,
+              }}
+            >
+              <Text
+                style={[
+                  commonStyles.yearsText,
+                  { alignSelf: "flex-start", paddingTop: 3 },
+                ]}
+              >
+                {`${item.initialYear} ${
+                  item.finalYear ? `- ${item.finalYear}` : ""
+                }`}
+              </Text>
+              <Text
+                style={[
+                  commonStyles.yearsAdditionalText,
+                  { textAlign: "justify", flex: 1 },
+                ]}
+              >
+                {item.description}
+              </Text>
+            </View>
+          </View>
+        ))}
+      </View>
+
+      <Text
+        style={commonStyles.pageNumber}
+        render={({ pageNumber, totalPages }) => `${pageNumber} / ${totalPages}`}
+        fixed
+      />
+    </React.Fragment>
+  );
+}
