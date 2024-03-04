dc9c623c1b745ef4550984ece4a9856d204676fb
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-03-04 20:07:58 -0300

    feat: update curriculum when delete new certificate

--- src/server/action/delete-certification-by-id.ts
+++ src/server/action/delete-certification-by-id.ts
@@ -5,12 +5,12 @@ import { unstable_noStore as noStore, revalidatePath } from "next/cache";
 import { deleteCertificationByIdSchema } from "@/validators/delete-certification-by-id";
 import { utapi } from "@/lib/upload-thing";
 import { db } from "../db/drizzle";
-import { certifications } from "../db/schema";
+import { certifications, curriculums } from "../db/schema";
 import { and, eq } from "drizzle-orm";
 
 export const deleteCertificationByIdAction = action(
   deleteCertificationByIdSchema,
-  async ({ key, userId }) => {
+  async ({ key, userId, curriculumId }) => {
     noStore();
     const deleteFromUploadThing = await utapi.deleteFiles(key);
 
@@ -18,18 +18,34 @@ export const deleteCertificationByIdAction = action(
       throw new Error("Failed to delete certificate");
     }
 
-    const [certificate] = await db
-      .delete(certifications)
-      .where(
-        and(eq(certifications.key, key), eq(certifications.userId, userId))
-      )
-      .returning();
+    await db.transaction(async (tx) => {
+      const [certificate] = await db
+        .delete(certifications)
+        .where(
+          and(eq(certifications.key, key), eq(certifications.userId, userId))
+        )
+        .returning();
 
-    if (!certificate) {
-      throw new Error("Failed to delete certificate");
-    }
+      if (!certificate) {
+        throw new Error("Failed to delete certificate");
+      }
 
-    revalidatePath("/edit-form");
+      const updateCurriculum = await tx
+        .update(curriculums)
+        .set({
+          statusCurriculum: "revision",
+        })
+        .where(
+          and(eq(curriculums.userId, userId), eq(curriculums.id, curriculumId))
+        )
+        .returning();
+
+      if (!updateCurriculum) {
+        throw new Error("Failed to update curriculum");
+      }
+
+      revalidatePath("/edit-form");
+    });
 
     return { message: `Certificação deletada.` };
   }
--- src/validators/delete-certification-by-id.ts
+++ src/validators/delete-certification-by-id.ts
@@ -3,4 +3,7 @@ import { z } from "zod";
 export const deleteCertificationByIdSchema = z.object({
   key: z.string().min(1, { message: "O id é obrigatório" }),
   userId: z.string().min(1, { message: "O id do usuário é obrigatório" }),
+  curriculumId: z
+    .number()
+    .min(1, { message: "O id do curriculo é obrigatório" }),
 });
