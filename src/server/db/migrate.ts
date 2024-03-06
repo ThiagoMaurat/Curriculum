a34e9e2db21f39d7b7127ff51ada00121c78a225
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-03-06 09:03:51 -0300

    fix:migrate types

--- src/server/db/migrate.ts
+++ src/server/db/migrate.ts
@@ -1,15 +1,18 @@
-import "dotenv/config";
-import { neonConfig } from "@neondatabase/serverless";
-import { migrate } from "drizzle-orm/neon-http/migrator";
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+// migrate.ts
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import 'dotenv/config'
 
-(async () => {
-  neonConfig.fetchConnectionCache = true;
-
-  const sql = neon(process.env.PG_CONNECTION_STRING!);
-  const db = drizzle(sql);
-  await migrate(db, { migrationsFolder: "./drizzle" });
-
-  console.log("Done");
-})();
+const databaseUrl = drizzle(postgres(`${process.env.PG_CONNECTION_STRING}`, 
+{ ssl: 'require', max: 1 }));
+const main = async () => {
+  try {
+    await migrate(databaseUrl, { migrationsFolder: 'drizzle' });
+    console.log('Migration complete');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit(0);
+};
+main();
