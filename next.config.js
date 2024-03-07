f506c02c100e01e067301dc2793dd9f91b5137e5
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-03-07 10:20:10 -0300

    feat: cache control ttf on public folder

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@t3-oss/env-nextjs", "bcryptjs"],
-  output: 'standalone',
+  output: "standalone",
+  headers() {
+    return [
+      {
+        //ttf on public nextjs
+        source: "/font/(.*).ttf",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, immutable",
+          },
+        ],
+      },
+      {
+        //woff on public nextjs
+        source: "/font/(.*).woff",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/components/templates/pdf-curriculum-template-admin.tsx
+++ src/components/templates/pdf-curriculum-template-admin.tsx
@@ -4,6 +4,7 @@ import FirstPage from "@/components/pdf-curriculum/pages/first-page";
 import { Document, Font } from "@react-pdf/renderer";
 import SecondPage from "@/components/pdf-curriculum/pages/second-page";
 import { ListTodoCurriculumByCollaborator } from "./forms-collaborator-create-curriculum";
+import { env } from "../../../env.mjs";
 
 interface PdfCurriculumTemplateProps {
   data: CurriculumFormInput;
@@ -14,20 +15,20 @@ Font.register({
   family: "Times New Roman",
   fonts: [
     {
-      src: "https://fonts.cdnfonts.com/s/57197/times.woff",
+      src: `${env.NEXT_PUBLIC_APP_URL}/font/times-new-roman/times.woff`,
       fontWeight: "normal",
     },
     {
-      src: "https://db.onlinewebfonts.com/t/7cc3599da04831aa8782e23d61cb18bb.ttf",
+      src: `${env.NEXT_PUBLIC_APP_URL}/font/times-new-roman/7cc3599da04831aa8782e23d61cb18bb.ttf`,
       fontWeight: "bold",
     },
     {
-      src: "https://db.onlinewebfonts.com/t/97fe1ef2f9f6cdac38897d731b88a774.ttf",
+      src: `${env.NEXT_PUBLIC_APP_URL}/font/times-new-roman/97fe1ef2f9f6cdac38897d731b88a774.ttf`,
       fontWeight: "normal",
       fontStyle: "italic",
     },
     {
-      src: "https://db.onlinewebfonts.com/t/de248931529da8d04cc09ee92cfb2a56.ttf",
+      src: `${env.NEXT_PUBLIC_APP_URL}/font/times-new-roman/de248931529da8d04cc09ee92cfb2a56.ttf`,
       fontWeight: "bold",
       fontStyle: "italic",
     },
@@ -38,11 +39,11 @@ Font.register({
   family: "Cambria",
   fonts: [
     {
-      src: "https://db.onlinewebfonts.com/t/758d40d7ca52e3a9bff2655c7ab5703c.ttf",
+      src: `${env.NEXT_PUBLIC_APP_URL}/font/cambria/758d40d7ca52e3a9bff2655c7ab5703c.ttf`,
       fontWeight: "normal",
     },
     {
-      src: "https://db.onlinewebfonts.com/t/2db80501ab27169c9b8395ce6f749be1.ttf",
+      src: `${env.NEXT_PUBLIC_APP_URL}/font/cambria/2db80501ab27169c9b8395ce6f749be1.ttf`,
       fontWeight: "bold",
     },
   ],
@@ -52,12 +53,8 @@ Font.register({
   family: "Calibri",
   fonts: [
     {
-      src: "https://db.onlinewebfonts.com/t/a78cfad3beb089a6ce86d4e280fa270b.ttf",
+      src: `${env.NEXT_PUBLIC_APP_URL}/font/calibri/a78cfad3beb089a6ce86d4e280fa270b.ttf`,
       fontWeight: "normal",
-    },
-    {
-      src: "https://db.onlinewebfonts.com/t/527c5ab608cab860a6aae8ce02e14b0e.ttf",
-      fontWeight: "bold",
     },
   ],
 });
