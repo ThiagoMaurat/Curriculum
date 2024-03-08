9de4be35da73714f89d44dff7c7211cd7998019f
ThiagoMaurat <thiagomaurat@hotmail.com>
2024-03-08 11:00:41 -0300

    fix:uploadthing error

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,7 +1,13 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
+import { env } from "../../../../env.mjs";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    uploadthingId: env.UPLOADTHING_APP_ID,
+    uploadthingSecret: env.UPLOADTHING_SECRET,
+    callbackUrl: env.NEXT_PUBLIC_APP_URL,
+  },
 });
